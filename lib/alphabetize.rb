4a7a073ecee6eaa6ccfedc1eeeddddc93b3e5b68
Stacey  Marks <staceyamarks@gmail.com>
2018-02-07 19:27:35 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase_str|
+    phrase_str.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
+
